4600ccb736b34f49438e8954508e70261618984f
Tariel <example@example.com>
2023-03-03 17:09:09 +0600

    Adding User and repository JPA infrastructure

---
+++ src/main/java/com/example/resumeportal/models/User.java
@@ -0,0 +1,47 @@
+package com.example.resumeportal.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String password;
+    private boolean active;
+    private String roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
