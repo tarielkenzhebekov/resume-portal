c4cb0dad5cc23bd44716ff4f564e32f57ba88b69
Tariel <example@example.com>
2023-03-04 17:13:48 +0600

    Added basic user profile elements and displayed in templates

--- src/main/java/com/example/resumeportal/model/UserProfile.java
+++ src/main/java/com/example/resumeportal/model/UserProfile.java
@@ -20,6 +20,21 @@ public class UserProfile {
     @Column(name = "SUMMARY")
     private String summary;
 
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "DESIGNATION")
+    private String designation;
+
     public int getId() {
         return id;
     }
@@ -50,5 +65,45 @@ public class UserProfile {
 
     public void setSummary(String summary) {
         this.summary = summary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO public."user" (ID, USER_NAME, PASSWORD, ACTIVE, ROLES) VALUES
-(1, 'user1', '123', 'true', 'USER'),
-(2, 'user2', '123', 'true', 'USER'),
-(3, 'user3', '123', 'true', 'USER');
+(1, 'tariel', '123', 'true', 'USER'),
+(2, 'shumkar', '123', 'true', 'USER'),
+(3, 'zhyrgal', '123', 'true', 'USER');
 
-INSERT INTO public."user_profile" (ID, USER_NAME, THEME, SUMMARY) VALUES
-(1, 'user1', 1, 'Summary of the user user1'),
-(2, 'user2', 2, 'Summary of the user user2'),
-(3, 'user3', 3, 'Summary of the user user3');
+INSERT INTO public."user_profile" (ID, USER_NAME, THEME, SUMMARY, FIRST_NAME, LAST_NAME, EMAIL, PHONE, DESIGNATION) VALUES
+(1, 'tariel', 1, 'Seeking a Backend Developer position with an outstanding career opportunity that will offer a rewarding work environment along with a winning team that will fully utilize management skills.', 'Tariel', 'Kenzhebekov', 'tariel@example.com', '0999-112-233', 'Back-End Developer'),
+(2, 'shumkar', 2, ' Junior Mobile Developer with 2 plus years of experience in the Telecommunications domain. Extensive mobile (IOS, Android &Hybrid) application development experience with strong knowledge of various programming languages.', 'Shumkarbek', 'Barpyev', 'shumkarbek@example.com', '0555-445-566', 'Mobile Developer'),
+(3, 'zhyrgal', 3, 'Front end developers use programming languages like HTML, JavaScript, and CSS, or frameworks like Tailwind, to determine and develop the visual concept and functionality of visual elements in a website.', 'Zhyrgalbek', 'Sharshenov', 'zhyrgalbek@example.com', '0777-889-977', 'Front-End Developer');
 
